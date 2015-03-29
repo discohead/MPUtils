55fa5ac34e0bafbc80c85815b993a48694d8d8d9
Jared McFarland <jaredcolin@gmail.com>
2015-03-29 02:23:06 -0700

    Initial Commit

---
+++ MPUtils/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MPUtils
+//
+//  Created by Jared McFarland on 3/29/15.
+//  Copyright (c) 2015 Jared McFarland. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MPUtils/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MPUtils
+//
+//  Created by Jared McFarland on 3/29/15.
+//  Copyright (c) 2015 Jared McFarland. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
